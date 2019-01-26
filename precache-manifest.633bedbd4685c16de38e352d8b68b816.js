32086464d533b2ef423c5a03829ce958935b0e94
Pavel Eremin <coolbig@bk.ru>
2019-01-27 00:46:51 +0300

    Updates

---
+++ precache-manifest.633bedbd4685c16de38e352d8b68b816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "693107f062d41af2bfce",
+    "url": "/test-tooltip/static/js/runtime~main.693107f0.js"
+  },
+  {
+    "revision": "801d6b7b3138e68d4a6f",
+    "url": "/test-tooltip/static/js/main.801d6b7b.chunk.js"
+  },
+  {
+    "revision": "bea3696eca004b1190be",
+    "url": "/test-tooltip/static/js/1.bea3696e.chunk.js"
+  },
+  {
+    "revision": "801d6b7b3138e68d4a6f",
+    "url": "/test-tooltip/static/css/main.f4810d66.chunk.css"
+  },
+  {
+    "revision": "af8373b35a5b0b89c11d6e6069177edf",
+    "url": "/test-tooltip/index.html"
+  }
+];
